3c5cf943b4f92c4184aab55b446ae8dc025e9bbf
MateusAparecido <72242588+MateusAparecido@users.noreply.github.com>
2022-02-10 02:22:23 -0300

    falta terminar de adptar kruskal e fluery,
    to com muito sono pra issos

--- Grafo.py
+++ Grafo.py
@@ -1,6 +1,4 @@
 #Implementa as funções para a biblioteca de Grafos nao direcionados ponderados
-from matplotlib import collections
-from collections import defaultdict
 
 class Grafo:
 
@@ -121,44 +119,132 @@ class Grafo:
         # armazenar o numero de componentes conectados
         count = 0
         temp = []
-
         for v in range(self.vertices+1):
             if (visited[v] == False):
+                #self.DFSUtil2(v, visited)
                 self._DFS(v, visited, temp)
                 count += 1
 
         return count-1
+
+
+     # Adiciona aresta (não direcionada)
+    def addEdge(self,u,v): 
+        self.graph[u].append(v) 
+        self.graph[v].append(u) 
+
+    #Função para remover a aresta u-v do grafo
+    def rmvEdge(self, u, v): 
+        for index, key in enumerate(self.graph[u]): 
+            if key == v: 
+                self.graph[u].pop(index) 
+        for index, key in enumerate(self.graph[v]): 
+            if key == u: 
+                self.graph[v].pop(index) 
+        ### Importante - auxiliar para verificar pontes ###
+  # Função baseada no DFS para contar os vértices alcançaveis por meio do vértice V.
+    def DFSCount(self, v, visited): 
+        count = 1
+        visited[v] = True
+        for i in self.graph[v]: 
+            if visited[i] == False: 
+                count = count + self.DFSCount(i, visited)        
+        return count 
+
+    # Função que verifica se a aresta u-v pode ser considerada uma aresta no Tour de Euler
+    def isValidNextEdge(self, u, v): 
+        # A aresta u-v é valida em um dos seguintes casos: 
+
+        # 1) Se v é o único vértice adjacente a u
+        if len(self.graph[u]) == 1: 
+            return True
+        else: 
+            ''' 
+            2) Caso existam múltiplos vértices adjacentes u-v
+            verificar se não é uma ponte
+                
+            Para checarmos se é uma ponte: 
+            2.a) contamos os vértices alcançaveis a partir de u'''  
+            visited =[False]*(self.V) 
+            count1 = self.DFSCount(u, visited) 
+
+            '''2.b) Remove a aresta (u, v) e após remover verificamos a quantidade de vértices alcançaveis a partir de u novamente'''
+            self.rmvEdge(u, v) 
+            visited =[False]*(self.V) 
+            count2 = self.DFSCount(u, visited) 
+
+            #2.c) Retornamos a aresta ao grafo, pois ela só foi removida para verificar se formavam-se pontes
+            self.addEdge(u,v) 
+
+            # 2.d) Se count1 é maior que count2, então a aresta (u, v) é uma ponte e não pode ser removida retornando falso.
+            return False if count1 > count2 else True
+
+
+    # Printa o tour de Euler tour começando pelo vértice u 
+    def printEulerUtil(self, u): 
+        #Recorre para todos os vértices adjacentes a este vértice
+        for v in self.graph[u]: 
+            #Se a vertice u-v não for removida e for uma próxima borda válida
+            if self.isValidNextEdge(u, v): 
+                print("%d-%d " %(u,v)), 
+                self.rmvEdge(u, v) 
+                self.printEulerUtil(v) 
+
+
     
-    #--------VERIFICAR SE UM GRAFO POSSUI CICLO-------#
+    '''The main function that print Eulerian Trail. It first finds an odd 
+    degree vertex (if there is any) and then calls printEulerUtil() 
+    to print the path '''
+    def printEulerTour(self): 
+        #encontre um vertice com grau impar 
+        u = 0
+        for i in range(self.V): 
+            if len(self.graph[i]) %2 != 0 : 
+                u = i 
+                break
+        # Imprimir tour começando do vértice ímpar 
+        print ("\n") 
+        self.printEulerUtil(u)
 
 
 
-    #----------DENSIDADE DO GRAFO------#
-    def densidade_grafo(self):
-        return self.tamanhoGrafo() / self.ordemGrafo()
-   
-
-    #-------DISTANCIA------#
-    def distancia(self, dist, menorcaminho):
-        min = 1e6
-        for v in range(self.ordemGrafo()):
-            if dist[v] < min and menorcaminho[v] ==False:
-                min = dist[v]
-                min_index = v 
-        return min_index
-    
-    '''
-    #----------CAMINHO-------#
-    def caminho(self, src):
-        src = src -1
-        dist = [1e6] * self.ordemGrafo()
-        dist[src] = 0 
-        menorcaminho = [False] * self.ordemGrafo()
-        for i in range(self.ordemGrafo()):
-            u = self.distancia(dist, menorcaminho)
-            menorcaminho[u] = True
-            for vertice in range (self.ordemGrafo()):
-                if (self.)
-    '''
-
+    #KRUSKAL
+    def search(self, parent, i):
+        if parent[i] == i:
+            return i
+        return self.search(parent, parent[i])
+ 
+    def apply_union(self, parent, rank, x, y):
+        xroot = self.search(parent, x)
+        yroot = self.search(parent, y)
+        if rank[xroot] < rank[yroot]:
+            parent[xroot] = yroot
+        elif rank[xroot] > rank[yroot]:
+            parent[yroot] = xroot
+        else:
+            parent[yroot] = xroot
+            rank[xroot] += 1
+ 
+  
+    def kruskal(self):
+        result = []
+        i, e = 0, 0
+        self.graph = sorted(self.graph, key=lambda item: item[2])
+        parent = []
+        rank = []
+        for node in range(self.V):
+            parent.append(node)
+            rank.append(0)
+        while e < self.V - 1:
+            u, v, w = self.graph[i]
+            i = i + 1
+            x = self.search(parent, u)
+            y = self.search(parent, v)
+            if x != y:
+                e = e + 1
+                result.append([u, v, w])
+                self.apply_union(parent, rank, x, y)
+        for u, v, weight in result:
+            print("Edge:",u, v,end =" ")
+            print("-",weight)
     
